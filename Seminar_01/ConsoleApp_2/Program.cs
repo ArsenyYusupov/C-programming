4ad68d58cc2a5daba7786a888519e95f3561a48d
Arseny <Syp4ik22885@mail.ru>
2022-10-20 16:33:17 +0300

    Task_02

---
+++ Seminar_01/ConsoleApp_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая на хвод принимает два числа и вычисляет не является ли одно квадратом другого
+Console.WriteLine("Введите число №1 ... ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2 ... ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine($"True: число №1 = {numberA} является квадратом числа №2 = {numberB}");
+}
+else
+{
+    Console.WriteLine($"False: число №1 = {numberA} НЕ является квадратом числа №2 = {numberB}");
+}
