932dbf6d05c38df2ee13c5124bde8a562c4fdecd
Arseny <Syp4ik22885@mail.ru>
2022-10-25 17:20:50 +0300

    Ex_03

---
+++ Seminar_02/Ex_02/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+var rand = new Random();
+int numbs = rand.Next(100, 1000);
+int first, last, result;
+Console.WriteLine(numbs);
+last = numbs % 10;
+first = numbs / 100;
+result = first*10 + last;
+
+Console.WriteLine($"Случайное число без второй цифры равно: {result}");
---
+++ Seminar_02/Ex_03/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+int numb1, numb2;
+string Out;
+
+Console.Write("Ввести первое число: ");
+numb1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ввести второе число: ");
+numb2 = int.Parse(Console.ReadLine());
+int result = numb1 % numb2;
+
+if(result != 0) Out = "не кратно, остаток " + result;
+else Out = "кратно";
+
+Console.WriteLine(Out);
