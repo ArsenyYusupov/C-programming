b862e94cb085c4e3db9c314c6ea2a8195e4b5656
Arseny <Syp4ik22885@mail.ru>
2022-11-09 21:01:04 +0300

    ex_07

---
+++ Seminar_03/Ex_04/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void PrintSquaresToConsole(int n)
+{
+    for (int i = 1; i <= n; i++ )
+    {
+       Console.WriteLine(i * i);
+    }
+}
+
+PrintSquaresToConsole(10);
