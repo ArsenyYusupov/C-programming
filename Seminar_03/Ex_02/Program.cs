1ae41bd94c635c285f3ff3788a68939faa54383b
Arseny <Syp4ik22885@mail.ru>
2022-11-09 18:51:01 +0300

    ex_03

--- Seminar_03/Ex_02/Program.cs
+++ Seminar_03/Ex_02/Program.cs
@@ -1,21 +1,37 @@
 ﻿//Напишите программу, которая по заданному номеру четверти,
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
-Console.WriteLine("Введите номер четверти:");
+//Console.WriteLine("Введите номер четверти:");
+//int n = Convert.ToInt32(Console.ReadLine());
+//if (n == 1)
+//{
+//  Console.WriteLine("x > 0, y > 0");
+//}
+//if (n == 2)
+//{
+//  Console.WriteLine("x < 0, y > 0");
+//}
+//if (n == 3)
+//{
+//  Console.WriteLine("x < 0, y < 0");
+//}
+//if (n == 4)
+//{
+//  Console.WriteLine("x > 0, y < 0");
+//}
+
+string GetCoordArea(int chetvert)
+{
+  string coords = "";
+  if (chetvert == 1) coords = "x > 0, y > 0";
+  if (chetvert == 2) coords = "x < 0, y > 0";
+  if (chetvert == 3) coords = "x < 0, y < 0";
+  if (chetvert == 4) coords = "x > 0, y < 0";
+  return coords;
+}
+
+Console.WriteLine("Vvedi nomer chetverti ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n == 1)
-{
-  Console.WriteLine("x > 0, y > 0");
-}
-if (n == 2)
-{
-  Console.WriteLine("x < 0, y > 0");
-}
-if (n == 3)
-{
-  Console.WriteLine("x < 0, y < 0");
-}
-if (n == 4)
-{
-  Console.WriteLine("x > 0, y < 0");
-}
+
+string coords = GetCoordArea(n);
+Console.WriteLine($"Vozmozhnie coordinati: {coords}");
---
+++ Seminar_03/Ex_03/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//int x1 = 0, y1 = 0;
+//int x2 = 0, y2 = 0;
+
+//double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+double ShowDistance(int x1, int y1, int x2, int y2)
+{
+  double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+  return distance;
+}
+
+Console.WriteLine("Введи Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи Х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double distance2 = ShowDistance(x1, y1, x2, y2);
+Console.WriteLine(distance2);
