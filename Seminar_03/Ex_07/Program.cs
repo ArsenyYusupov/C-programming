8a4222728ac7321a41213ce614c4a8fd6d63c399
Arseny <Syp4ik22885@mail.ru>
2022-11-14 16:37:44 +0300

    ex_07

--- Seminar_03/Ex_07/Program.cs
+++ Seminar_03/Ex_07/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.WriteLine("Введите число: ");
+
+/*Console.WriteLine("Введите число: ");
 string number = Console.ReadLine();
 int len = number.Length;
 
@@ -18,3 +19,29 @@ else
 {
     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
 }
+*/
+// Принимает на вход 5-ое число
+bool IsPolindrome(int number)
+{
+   string strNumber = number.ToString();
+   
+   if (strNumber[0] == strNumber[4] && strNumber[1] == strNumber[3])
+   {
+    return true;
+   }
+   
+    return false;
+}
+
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 9999 || number >= 100000)
+{
+    Console.WriteLine("Ввели некорректное число!(положительное)");
+    return;
+}
+
+bool isPolindrome = IsPolindrome(number);
+Console.WriteLine(isPolindrome);
