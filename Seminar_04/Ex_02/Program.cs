e26010ff65d003ea8c81728044c0ac26e3fde780
Arseny <Syp4ik22885@mail.ru>
2022-11-14 18:22:02 +0300

    ex_02

---
+++ Seminar_04/Ex_02/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strNumber = Math.Abs(number).ToString();
+Console.WriteLine(strNumber.Length);
