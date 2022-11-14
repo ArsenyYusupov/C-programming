41647df263f3b37929d7b0b525d23ee4624e3cf3
Arseny <Syp4ik22885@mail.ru>
2022-11-14 18:32:24 +0300

    ex_04

---
+++ Seminar_04/Ex_03/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int GetSum(int number)
+{
+  int sum = 1;
+  
+  /*int tempValue = 1;
+  while (tempValue <= number)
+  {
+   sum = sum + tempValue;
+   tempValue = tempValue + 1;
+  }*/
+   for (int i = 1; i <= number; i++)
+   {
+     sum = sum * i;
+   }
+
+  return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = GetSum(number);
+Console.WriteLine(sum);
