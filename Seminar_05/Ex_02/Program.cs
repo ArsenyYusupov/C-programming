70fcc919e655b606849f3b518e8b94eece1e1003
Arseny <Syp4ik22885@mail.ru>
2022-11-22 13:40:11 +0300

    ex_02

---
+++ Seminar_05/Ex_02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные и наоборот.
+// boll Search(int[ sourceArray], int value)
+int[] array = new int[8] {4, 3, 7, 5, 9, 1, 6, 0};
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write(array[i] + " ");
+}
