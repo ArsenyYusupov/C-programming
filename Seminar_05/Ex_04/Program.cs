99d4742eaeb3a6b648ac7c18d8b2ed7f8b4e1da0
Arseny <Syp4ik22885@mail.ru>
2022-11-22 16:39:57 +0300

    ex_04

---
+++ Seminar_05/Ex_04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найдите количество элементов массива 123 [10, 99]
+int[] array = new int[123];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 999);
+    Console.Write(array[i] + " ");
+
+}
+
+int Count(int[] array, int a1, int a2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > a1 && array[i] < a2)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите число: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество нужных нам элементов = {Count(array, a1, a2)}");
+
