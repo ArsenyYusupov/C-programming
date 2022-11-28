cdb5be393f9d822478105e7594dcb8034d645139
Arseny <Syp4ik22885@mail.ru>
2022-11-29 01:33:51 +0300

    ex_01

---
+++ Seminar_06/Ex_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу которая перевернет массив.
+
+int[] array = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[(array.Length - 1) - i];
+    array[(array.Length - 1) - i] = temp;
+}
