0342169bf2cb8d83be9a4505be18cb4414516b66
Arseny <Syp4ik22885@mail.ru>
2022-11-29 20:38:27 +0300

    ex_02

---
+++ Seminar_06/Ex_02/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.Write("Введите число: ");
+int dec = Convert.ToInt32(Console.ReadLine());
+string ConvertToBin(int a)
+{
+    string bin1 = Convert.ToString(a);
+    string bin2 = "";
+    while (a > 0)
+    {
+        bin2 = (a % 2) + bin2;
+        a = a / 2;
+    }
+    return bin2;
+}
+Console.WriteLine(ConvertToBin(dec));
